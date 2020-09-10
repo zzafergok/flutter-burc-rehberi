a37de814e4fd6f8eea14ae5ef94fafc87f1aed3d
zzafergok <zfrgk96@gmail.com>
2020-09-10 19:40:12 +0300

    using palatte generator and project done

--- lib/burc_detay.dart
+++ lib/burc_detay.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_burc_rehberi/burc_liste.dart';
 import 'models/burc.dart';
+import 'package:palette_generator/palette_generator.dart';
 
-class BurcDetay extends StatelessWidget {
+class BurcDetay extends StatefulWidget {
   int gelenIndex;
-  Burc secilenBurc;
 
   BurcDetay(this.gelenIndex);
 
   @override
+  _BurcDetayState createState() => _BurcDetayState();
+}
+
+class _BurcDetayState extends State<BurcDetay> {
+  Burc secilenBurc;
+  Color baskinRenk;
+  PaletteGenerator paletteGenerator;
+
+  @override
+  void initState() {
+    super.initState();
+    secilenBurc = BurcListe.tumBurclar[widget.gelenIndex];
+    baskinRengiBul();
+  }
+
+  void baskinRengiBul() {
+    Future<PaletteGenerator> fPaletGenerator =
+        PaletteGenerator.fromImageProvider(
+            AssetImage("images/" + secilenBurc.burcBuyukResim));
+
+    fPaletGenerator.then((value) {
+      paletteGenerator = value;
+
+      setState(() {
+        baskinRenk = paletteGenerator.dominantColor.color;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    secilenBurc = BurcListe.tumBurclar[gelenIndex];
     return Scaffold(
       body: CustomScrollView(
         primary: false,
@@ -19,12 +48,15 @@ class BurcDetay extends StatelessWidget {
             expandedHeight: 250,
             pinned: true,
             primary: true,
-            backgroundColor: Colors.green,
+            backgroundColor: baskinRenk != null ? baskinRenk : Colors.green,
             flexibleSpace: FlexibleSpaceBar(
               background: Image.asset("images/" + secilenBurc.burcBuyukResim,
                   fit: BoxFit.cover),
               centerTitle: true,
-              title: Text(secilenBurc.burcAdi + "Burcu ve Özellikleri"),
+              title: Text(
+                secilenBurc.burcAdi + " Burcu ve Özellikleri",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           ),
           SliverToBoxAdapter(
